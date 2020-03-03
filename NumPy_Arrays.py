060de3f5c569c5251dd1aadc823c3022df43f33a
AME <adrien.mechin@gmail.com>
2020-03-03 13:37:59 +0100

    Arrays Exercices

--- NumPy_Arrays.py
+++ NumPy_Arrays.py
@@ -3,5 +3,5 @@ import numpy as np
 distances = [10, 15, 17, 26, 20]
 times = [0.3, 0.47, 0.55, 1.20, 1.0]
 
-distances = np.array(distances)
-times = np.array(times)
+# distances = np.array(distances)
+# times = np.array(times)
